79770666315f70c32fc79a36c98796a35f58c2fe
pq <pq.blogspot@gmail.com>
2017-10-10 00:25:55 +0900

    HM

--- SpreadSheetExample/src/tcucalc.py
+++ SpreadSheetExample/src/tcucalc.py
@@ -1,7 +1,6 @@
 #!/opt/libreoffice5.4/program/python
 # -*- coding: utf-8 -*-
 import unohelper  # オートメーションには必須(必須なのはuno)。
-from com.sun.star.awt import Rectangle
 def macro():
 	ctx = XSCRIPTCONTEXT.getComponentContext()  # コンポーネントコンテクストの取得。
 	smgr = ctx.getServiceManager()  # サービスマネージャーの取得。	
