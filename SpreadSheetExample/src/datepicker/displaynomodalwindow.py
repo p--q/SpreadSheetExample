56dd6bc119023cf996e31001b93b23f4862af716
pq <pq.blogspot@gmail.com>
2018-01-23 00:12:33 +0900

    HM

--- SpreadSheetExample/src/datepicker/displaynomodalwindow.py
+++ SpreadSheetExample/src/datepicker/displaynomodalwindow.py
@@ -15,6 +15,12 @@ def macro(documentevent=None):  # 引数は文書のイベント駆動用。
 	smgr = ctx.getServiceManager()  # サービスマネージャーの取得。
 	controller = doc.getCurrentController()  # コントローラの取得。
 	controller.addEnhancedMouseClickHandler(EnhancedMouseClickHandler(controller, ctx, smgr, doc))  # EnhancedMouseClickHandler
+
+	frame = controller.getFrame()  # モデル→コントローラ→フレーム、でドキュメントのフレームを取得。
+	componentwindow = frame.getComponentWindow()  # コンポーネントウィンドウを取得。
+	pass
+
+
 class EnhancedMouseClickHandler(unohelper.Base, XEnhancedMouseClickHandler):
 	def __init__(self, subj, ctx, smgr, doc):
 		self.subj = subj
@@ -152,3 +158,51 @@ def dialogCreator(ctx, smgr, dialogprops):  # ダイアログと、それにコ�
 		return name
 	return dialog, addControl  # コントロールコンテナとそのコントロールコンテナにコントロールを追加する関数を返す。
 g_exportedScripts = macro, #マクロセレクターに限定表示させる関数をタプルで指定。
+if __name__ == "__main__":  # オートメーションで実行するとき
+	import officehelper
+	from functools import wraps
+	import sys
+	from com.sun.star.beans import PropertyValue
+	from com.sun.star.script.provider import XScriptContext  
+	def connectOffice(func):  # funcの前後でOffice接続の処理
+		@wraps(func)
+		def wrapper():  # LibreOfficeをバックグラウンドで起動してコンポーネントテクストとサービスマネジャーを取得する。
+			try:
+				ctx = officehelper.bootstrap()  # コンポーネントコンテクストの取得。
+			except:
+				print("Could not establish a connection with a running office.", file=sys.stderr)
+				sys.exit()
+			print("Connected to a running office ...")
+			smgr = ctx.getServiceManager()  # サービスマネジャーの取得。
+			print("Using {} {}".format(*_getLOVersion(ctx, smgr)))  # LibreOfficeのバージョンを出力。
+			return func(ctx, smgr)  # 引数の関数の実行。
+		def _getLOVersion(ctx, smgr):  # LibreOfficeの名前とバージョンを返す。
+			cp = smgr.createInstanceWithContext('com.sun.star.configuration.ConfigurationProvider', ctx)
+			node = PropertyValue(Name = 'nodepath', Value = 'org.openoffice.Setup/Product' )  # share/registry/main.xcd内のノードパス。
+			ca = cp.createInstanceWithArguments('com.sun.star.configuration.ConfigurationAccess', (node,))
+			return ca.getPropertyValues(('ooName', 'ooSetupVersion'))  # LibreOfficeの名前とバージョンをタプルで返す。
+		return wrapper
+	@connectOffice  # mainの引数にctxとsmgrを渡すデコレータ。
+	def main(ctx, smgr):  # XSCRIPTCONTEXTを生成。
+		class ScriptContext(unohelper.Base, XScriptContext):
+			def __init__(self, ctx):
+				self.ctx = ctx
+			def getComponentContext(self):
+				return self.ctx
+			def getDesktop(self):
+				return ctx.getByName('/singletons/com.sun.star.frame.theDesktop')  # com.sun.star.frame.Desktopはdeprecatedになっている。
+			def getDocument(self):
+				return self.getDesktop().getCurrentComponent()
+		return ScriptContext(ctx)  
+	XSCRIPTCONTEXT = main()  # XSCRIPTCONTEXTを取得。
+	doc = XSCRIPTCONTEXT.getDocument()  # 現在開いているドキュメントを取得。
+	doctype = "scalc", "com.sun.star.sheet.SpreadsheetDocument"  # Calcドキュメントを開くとき。
+# 	doctype = "swriter", "com.sun.star.text.TextDocument"  # Writerドキュメントを開くとき。
+	if (doc is None) or (not doc.supportsService(doctype[1])):  # ドキュメントが取得できなかった時またはCalcドキュメントではない時
+		XSCRIPTCONTEXT.getDesktop().loadComponentFromURL("private:factory/{}".format(doctype[0]), "_blank", 0, ())  # ドキュメントを開く。ここでdocに代入してもドキュメントが開く前にmacro()が呼ばれてしまう。
+	flg = True
+	while flg:
+		doc = XSCRIPTCONTEXT.getDocument()  # 現在開いているドキュメントを取得。
+		if doc is not None:
+			flg = (not doc.supportsService(doctype[1]))  # ドキュメントタイプが確認できたらwhileを抜ける。
+	macro()
