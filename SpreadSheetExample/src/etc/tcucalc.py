4c1e1bc65916b61e596e6886b3f57d07cb2cad92
pq <pq.blogspot@gmail.com>
2018-03-02 19:18:38 +0900

    HM

--- SpreadSheetExample/src/etc/tcucalc.py
+++ SpreadSheetExample/src/etc/tcucalc.py
@@ -13,9 +13,11 @@ def macro():
 	ctx = XSCRIPTCONTEXT.getComponentContext()  # コンポーネントコンテクストの取得。
 	smgr = ctx.getServiceManager()  # サービスマネージャーの取得。 
 	tcu = smgr.createInstanceWithContext("pq.Tcu", ctx)  # サービス名か実装名でインスタンス化。
-	unocontroldialogmodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialogModel", ctx)  # コントロールダイアログモデル。
-	unocontrolcontainermodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlContainerModel", ctx)  # コントロールコンテナモデル。
-	tcu.wcompare(unocontroldialogmodel, unocontrolcontainermodel)
+# 	unocontroldialogmodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialogModel", ctx)  # コントロールダイアログモデル。
+# 	unocontrolcontainermodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlContainerModel", ctx)  # コントロールコンテナモデル。
+# 	tcu.wcompare(unocontroldialogmodel, unocontrolcontainermodel)
+
+# 	print("\n".join(tcu.tree(unocontroldialogmodel)))
 
 
 # 	tcu.wtree(XSCRIPTCONTEXT)
@@ -32,7 +34,7 @@ def macro():
 # 	
 # 	tdm.getByHierarchicalName(idl)
 	
-	doc = XSCRIPTCONTEXT.getDocument()
+# 	doc = XSCRIPTCONTEXT.getDocument()
 # 	controller = doc.getCurrentController()  # コントローラの取得。 
 # 	sheet = controller.getActiveSheet()  # アクティブなシートを取得。
 # 	columnlabelranges = doc.getPropertyValue("ColumnLabelRanges") 
@@ -158,9 +160,9 @@ def macro():
 # 	cellrangeaddress = doc.createInstance("com.sun.star.sheet.SheetCellRanges")  # com.sun.star.sheet.SheetCellRangesをインスタンス化。
 # 	tcu.wtree(cellrangeaddress)
 	
-# 	doc = XSCRIPTCONTEXT.getDocument()  # Calcドキュメント。
-# 	controller = doc.getCurrentController()  # コントローラの取得。
-# 	sheet = controller.getActiveSheet()  # アクティブなシートを取得。
+	doc = XSCRIPTCONTEXT.getDocument()  # Calcドキュメント。
+	controller = doc.getCurrentController()  # コントローラの取得。
+	sheet = controller.getActiveSheet()  # アクティブなシートを取得。
 # 	cell = sheet["C1"]
 # 	address = cell.getCellAddress()
 # 	annotations = sheet.getAnnotations()
@@ -256,7 +258,7 @@ def macro():
 
 # 	tcu.wcompare(sheet["A1"], sheet[0, 0])  # どちらもセル。
 # 	tcu.wcompare(sheet["A1"], sheet[0:1, 0])  # セルとセル範囲
-# 	tcu.wcompare(sheet[0, 0], sheet[0:1, 0])  # セルとセル範囲
+	tcu.wcompare(sheet[0, 0], sheet[0:1, 0])  # セルとセル範囲
 
 # 	print(sheet["A1"])
 # 	print(sheet[0, 0])
